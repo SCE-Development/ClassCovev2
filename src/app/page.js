343f55484cc991c6d9e4bf378c4cc2e67fadb2df
pnavab <114110926+pnavab@users.noreply.github.com>
2023-09-03 21:06:24 -0700

    Add supabase and rudimentary supabase user auth

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+import AuthForm from "./auth-form";
+
+export default function Home() {
+  return (
+    <div className="row">
+      <div className="col-6">
+        <h1 className="header">Supabase Auth + Storage</h1>
+        <p>
+          Testing out auth and storage
+        </p>
+      </div>
+      <div className="col-6 auth-widget">
+        <AuthForm />
+      </div>
+    </div>
+  )
+}
---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+import { NextRequest } from 'next/server'
+import { Database } from './types/supabase'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient<Database>({ req, res })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  //if the user is signed in and current path is / redirect the user to /account
+  if (user && req.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/account', req.url))
+  }
+
+  // user is not signed in and the current path is not / redirect the user to /
+  if (!user && req.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: ['/', '/account'],
+}
